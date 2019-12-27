c144dde8a8a3ee8ea5fdd595e7848a956dd237be
liuyan <liuyan@sogou-inc.com>
2019-12-27 23:04:01 +0800

    add std::future test

---
+++ std_async/std_async.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <future>
+
+int anotherFunction(const std::wstring &name) {
+    std::wcout << L"Hello, " << name << std::endl;
+}
+
+int main(int argc, const char **argv) {
+    std::async(std::launch::async, anotherFunction, L"liuyan");
+    return 0;
+}
